d586002597acb60367eeabad8530da66209773ef
512391 <512391@bsd48.org>
2025-02-26 10:05:01 -0800

    Add files via upload

--- ClawCapture.ino
+++ ClawCapture.ino
@@ -1,5 +1,9 @@
 #include <MeMegaPi.h>
-#include "MeMegaPi.h"
+
+const int CLAW_SPEED = 60;
+const int MOTOR_SPEED = 100;
+const int ARM_SPEED = 50;
+const int INTERMEDIATE_DELAY = 1000;
 
 MeUltrasonicSensor ultraSensor(PORT_8);
 MeEncoderOnBoard Encoder_1(SLOT1);
@@ -15,7 +19,7 @@ void isr_process_encoder1(void)
   }
   else
   {
-    Encoder_1.pulsePosPlus();;
+    Encoder_1.pulsePosPlus();
   }
 }
 
@@ -43,11 +47,79 @@ void isr_process_encoder3(void)
   }
 }
 
+void openClaw()
+{
+  claw.run(CLAW_SPEED);
+  delay(6000);
+  claw.stop();
+}
+
+void closeClaw()
+{
+  claw.run(-CLAW_SPEED);
+  delay(6000);
+  claw.stop();
+}
+
+void raiseClaw()
+{
+  Encoder_3.setMotorPwm(-ARM_SPEED);
+  delay(3000);
+  Encoder_3.setMotorPwm(0);
+}
+
+void lowerClaw()
+{
+  Encoder_3.setMotorPwm(ARM_SPEED);
+  delay(2700);
+  Encoder_3.setMotorPwm(0);
+}
+
+void moveStraight(bool back)
+{
+  if(!back)
+  {
+    Encoder_1.setMotorPwm(-MOTOR_SPEED);
+    Encoder_2.setMotorPwm(MOTOR_SPEED);
+    delay(1500);
+    Encoder_1.setMotorPwm(0);
+    Encoder_2.setMotorPwm(0);
+  }
+  else
+  {
+    Encoder_1.setMotorPwm(MOTOR_SPEED);
+    Encoder_2.setMotorPwm(-MOTOR_SPEED);
+    delay(1500);
+    Encoder_1.setMotorPwm(0);
+    Encoder_2.setMotorPwm(0);
+  }
+}
+
+void rotate(bool back)
+{
+  if(!back)
+  {
+    Encoder_1.setMotorPwm(MOTOR_SPEED);
+    Encoder_2.setMotorPwm(MOTOR_SPEED);
+    delay(3000);
+    Encoder_1.setMotorPwm(0);
+    Encoder_2.setMotorPwm(0);
+  }
+  else
+  {
+    Encoder_1.setMotorPwm(-MOTOR_SPEED);
+    Encoder_2.setMotorPwm(-MOTOR_SPEED);
+    delay(3000);
+    Encoder_1.setMotorPwm(0);
+    Encoder_2.setMotorPwm(0);
+  }
+}
+
 void setup()
 {
   attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
   attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
-  attachInterrupt(Encoder_3.getIntNum(), isr_process_encoder2, RISING);
+  attachInterrupt(Encoder_3.getIntNum(), isr_process_encoder3, RISING);
   Serial.begin(115200);
   
   //Set PWM 8KHz
@@ -56,9 +128,18 @@ void setup()
 
   TCCR2A = _BV(WGM21) | _BV(WGM20);
   TCCR2B = _BV(CS21);
+
+  Encoder_1.setPulse(7);
+  Encoder_2.setPulse(7);
+  Encoder_1.setRatio(26.9);
+  Encoder_2.setRatio(26.9);
+  Encoder_1.setPosPid(1.8, 0, 1.2);
+  Encoder_2.setPosPid(1.8, 0, 1.2);
+  Encoder_1.setSpeedPid(0.18, 0, 0);
+  Encoder_2.setSpeedPid(0.18, 0, 0);
 }
 
-bool running = false;
+bool hasGrabbed = false;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -66,15 +147,27 @@ void loop() {
   Serial.print(ultraSensor.distanceCm());
   Serial.println(" cm");
   delay(100);
-
-  if(ultraSensor.distanceCm() < 15 && !running)
+  openClaw();
+  while(true)
   {
-    running = true;
-    claw.run(-50);
-    running = false;
-  }
-  else
-  {
-    claw.stop()
+    if(ultraSensor.distanceCm() < 10)
+    {
+      closeClaw();
+      delay(INTERMEDIATE_DELAY);
+      raiseClaw();
+      delay(INTERMEDIATE_DELAY);
+      moveStraight(false);
+      delay(INTERMEDIATE_DELAY);
+      rotate(true);
+      delay(INTERMEDIATE_DELAY);
+      lowerClaw();
+      delay(INTERMEDIATE_DELAY);
+      openClaw();
+      delay(INTERMEDIATE_DELAY);
+      rotate(false);
+      delay(INTERMEDIATE_DELAY);
+      moveStraight(true);
+      delay(INTERMEDIATE_DELAY);
+    }
   }
 }
